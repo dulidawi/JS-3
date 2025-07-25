ffe0d4ecaf6459c29d5370c057b7957091079195
Oleksandra <157833785+dulidawi@users.noreply.github.com>
2025-07-25 20:31:18 +0200

    1234

---
+++ js/lesson3.js
@@ -0,0 +1,18 @@
+const result = 5 + 5 + '5' 
+console.log(typeof result);
+
+const email = "dulidawi@gmail.com"
+console.log(email.includes("@"));
+console.log(email.length);
+
+const word1 = "My"
+const word2 = "name"
+const word3 = "is"
+const fullName = word1 + word2 + word3 + "Victor"
+console.log(fullName);
+
+
+const userName = "Oleksandra"
+const payment = 400
+console.log(alert(`Дякуємо, ${userName}! До сплати ${payment} гривень!`));
+
